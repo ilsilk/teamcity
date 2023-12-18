5724e4bf70cf8c1379776c6ef3eba102ab57cbba
Ilia Shelkovyi <ilia.shelkovyi@semrush.com>
2023-12-18 13:30:25 +0100

    Create build type step fix

--- src/main/java/com/teamcity/ui/pages/admin/CreateBuildTypeStepPage.java
+++ src/main/java/com/teamcity/ui/pages/admin/CreateBuildTypeStepPage.java
@@ -7,16 +7,18 @@ import com.teamcity.api.generators.RandomData;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static com.teamcity.ui.Selectors.byDataTest;
 
 public class CreateBuildTypeStepPage extends EditBuildTypePage {
 
     private static final String NEW_BUILD_STEP_URL = "/admin/editRunType.html?id=buildType:%s&runnerId=__NEW_RUNNER__";
     private static final String COMMAND_LINE_RUNNER_TYPE = "Command Line";
-    private static final String CUSTOM_SCRIPT_SELECTOR = "[id='script.content']";
     private final SelenideElement runnerItemFilterInput = $(byDataTest("runner-item-filter"));
     private final SelenideElement buildStepNameInput = $("#buildStepName");
+    private final SelenideElement customScriptLine = $(".CodeMirror-code");
+    private final SelenideElement customScriptInput = $(".CodeMirror textarea");
     private final ElementsCollection runnerItems = $$(byDataTest("runner-item"));
 
     public CreateBuildTypeStepPage() {
@@ -30,7 +32,8 @@ public class CreateBuildTypeStepPage extends EditBuildTypePage {
     public void createCommandLineBuildStep(String customScript) {
         runnerItems.findBy(text(COMMAND_LINE_RUNNER_TYPE)).click();
         buildStepNameInput.shouldBe(visible, BASE_WAITING).val(RandomData.getString());
-        executeJavaScript("document.querySelector('%s').value = '%s'".formatted(CUSTOM_SCRIPT_SELECTOR, customScript));
+        customScriptLine.click();
+        customScriptInput.sendKeys(customScript);
         submitButton.click();
     }
 
