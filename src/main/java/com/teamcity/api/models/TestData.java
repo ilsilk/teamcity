383bc83e8382b8e3baa4f69c04bdaaf361cd7717
Ilia Shelkovyi <ilia.shelkovyi@semrush.com>
2024-09-05 21:58:51 +0200

    Eliminating casting and using generics for test data

--- src/main/java/com/teamcity/api/models/TestData.java
+++ src/main/java/com/teamcity/api/models/TestData.java
@@ -3,6 +3,8 @@ package com.teamcity.api.models;
 import lombok.Data;
 
 @Data
+// Набор тестовых данных, используемых в тестах. Генерируется с помощью TestDataGenerator.generate
+// Порядок определения важен и влияет на этот метод
 public class TestData {
 
     private NewProjectDescription project;
