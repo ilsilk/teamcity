6d6ccd8ce7b6c9ac9b744cf57a0a08e9ed507c20
Ilia Shelkovyi <ilia.shelkovyi@semrush.com>
2024-09-11 10:51:27 +0200

    Add generics to CheckedRequests

--- src/main/java/com/teamcity/api/requests/CheckedRequests.java
+++ src/main/java/com/teamcity/api/requests/CheckedRequests.java
@@ -1,6 +1,7 @@
 package com.teamcity.api.requests;
 
 import com.teamcity.api.enums.Endpoint;
+import com.teamcity.api.models.BaseModel;
 import com.teamcity.api.requests.checked.CheckedBase;
 import io.restassured.specification.RequestSpecification;
 import lombok.Getter;
@@ -19,8 +20,9 @@ public final class CheckedRequests {
         }
     }
 
-    public CheckedBase<?> getRequest(Endpoint endpoint) {
-        return checkedRequests.get(endpoint);
+    @SuppressWarnings("unchecked")
+    public <T extends BaseModel> CheckedBase<T> getRequest(Endpoint endpoint) {
+        return (CheckedBase<T>) checkedRequests.get(endpoint);
     }
 
 }
