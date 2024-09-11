323e9b73cb0879812b5c096e899be7b0fb488dbd
Ilia Shelkovyi <ilia.shelkovyi@semrush.com>
2024-09-11 11:20:49 +0200

    Add generics to CheckedRequests

--- src/test/java/com/teamcity/api/SearchTest.java
+++ src/test/java/com/teamcity/api/SearchTest.java
@@ -4,7 +4,6 @@ import com.teamcity.api.models.BuildType;
 import com.teamcity.api.models.Project;
 import com.teamcity.api.models.User;
 import com.teamcity.api.requests.checked.CheckedBase;
-import com.teamcity.api.spec.Specifications;
 import io.qameta.allure.Feature;
 import org.testng.annotations.Test;
 
@@ -19,12 +18,9 @@ import static com.teamcity.api.generators.TestDataGenerator.generate;
 public class SearchTest extends BaseApiTest {
 
     private static final int CREATED_MODELS_COUNT = 3;
-    private final CheckedBase<Project> checkedProjectRequest = new CheckedBase<>(Specifications.getSpec()
-            .superUserSpec(), PROJECTS);
-    private final CheckedBase<User> checkedUserRequest = new CheckedBase<>(Specifications.getSpec()
-            .superUserSpec(), USERS);
-    private final CheckedBase<BuildType> checkedBuildTypeRequest = new CheckedBase<>(Specifications.getSpec()
-            .superUserSpec(), BUILD_TYPES);
+    private final CheckedBase<Project> checkedProjectRequest = checkedSuperUser.getRequest(PROJECTS);
+    private final CheckedBase<User> checkedUserRequest = checkedSuperUser.getRequest(USERS);
+    private final CheckedBase<BuildType> checkedBuildTypeRequest = checkedSuperUser.getRequest(BUILD_TYPES);
 
     @Test(description = "User should be able to search models", groups = {"Regression"})
     public void searchTest() {
