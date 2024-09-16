db3c19687a2af23bbcde276b59f888e29002918a
Ilia Shelkovyi <ilia.shelkovyi@semrush.com>
2024-09-16 23:12:21 +0200

    Fix SoftAssertions

--- src/test/java/com/teamcity/BaseTest.java
+++ src/test/java/com/teamcity/BaseTest.java
@@ -6,15 +6,13 @@ import com.teamcity.api.requests.CheckedRequests;
 import com.teamcity.api.requests.UncheckedRequests;
 import com.teamcity.api.spec.Specifications;
 import org.assertj.core.api.SoftAssertions;
-import org.testng.IHookCallBack;
-import org.testng.IHookable;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import static com.teamcity.api.generators.TestDataGenerator.generate;
 
-public class BaseTest implements IHookable {
+public class BaseTest {
 
     protected final CheckedRequests checkedSuperUser = new CheckedRequests(Specifications.getSpec().superUserSpec());
     protected final UncheckedRequests uncheckedSuperUser = new UncheckedRequests(Specifications.getSpec().superUserSpec());
@@ -25,28 +23,18 @@ public class BaseTest implements IHookable {
     public void generateBaseTestData() {
         // Генерируем одну testData перед каждым тестом (так как она всегда нужна), без добавления ее в какое-то хранилище
         testData = generate();
+        softy = new SoftAssertions();
     }
 
     @AfterMethod(alwaysRun = true)
-    public void deleteCreatedEntities() {
-        TestDataStorage.getStorage().deleteCreatedEntities();
-    }
-
-    // Если делать assertAll в @AfterMethod, то ничего не будет работать: тест не будет ретраиться, а все последующие тесты в его классе скипнутся.
-    // Это происходит, потому что в таком случае фейлится не сам тест, а его After метод.
-    // С помощью данного хука запускаем assertAll в конце каждого теста и передаем фейл в сам тест.
-    @Override
-    public void run(IHookCallBack callBack, ITestResult testResult) {
-        softy = new SoftAssertions();
-        testResult.setAttribute("softy", softy);
-        callBack.runTestMethod(testResult);
-        softy = (SoftAssertions) testResult.getAttribute("softy");
+    public void deleteCreatedEntities(ITestResult testResult) {
         try {
             softy.assertAll();
         } catch (AssertionError e) {
             testResult.setThrowable(e);
             testResult.setStatus(ITestResult.FAILURE);
         }
+        TestDataStorage.getStorage().deleteCreatedEntities();
     }
 
 }
--- src/test/java/com/teamcity/common/RetryListener.java
+++ src/test/java/com/teamcity/common/RetryListener.java
@@ -1,5 +1,6 @@
 package com.teamcity.common;
 
+import com.teamcity.ui.BaseUiTest;
 import org.testng.IAnnotationTransformer;
 import org.testng.annotations.ITestAnnotation;
 
@@ -12,7 +13,7 @@ public final class RetryListener implements IAnnotationTransformer {
     @Override
     public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
         annotation.setRetryAnalyzer(RetryAnalyzer.class);
-        if (testMethod.getDeclaringClass().getName().startsWith("com.teamcity.ui")) {
+        if (BaseUiTest.class.isAssignableFrom(testMethod.getDeclaringClass())) {
             annotation.setDataProvider("browserProvider");
         }
     }
