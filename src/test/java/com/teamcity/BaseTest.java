b376aaefeb1b30af27ddb083536eff80f6cf5700
Ilia Shelkovyi <ilia.shelkovyi@semrush.com>
2024-09-16 21:53:26 +0200

    Fix SoftAssertions

--- src/test/java/com/teamcity/BaseTest.java
+++ src/test/java/com/teamcity/BaseTest.java
@@ -32,9 +32,9 @@ public class BaseTest implements IHookable {
         TestDataStorage.getStorage().deleteCreatedEntities();
     }
 
-    // Если делать assertAll в @AfterMethod, то ничего не будет работать: тест не будет ретраиться, а все последующие тесты в этом классе скипнутся
-    // Это происходит, потому что в таком случае фейлится не сам тест, а After метод
-    // С помощью данного хука запускаем assertAll в конце каждого теста и передаем фейл в сам тест
+    // Если делать assertAll в @AfterMethod, то ничего не будет работать: тест не будет ретраиться, а все последующие тесты в его классе скипнутся.
+    // Это происходит, потому что в таком случае фейлится не сам тест, а его After метод.
+    // С помощью данного хука запускаем assertAll в конце каждого теста и передаем фейл в сам тест.
     @Override
     public void run(IHookCallBack callBack, ITestResult testResult) {
         softy = new SoftAssertions();
